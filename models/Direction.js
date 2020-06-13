cc0cdad160f0eaf2f6c598b25ce8647c5ae9c7a0
KC Kai <kirstenckai@gmail.com>
2020-06-12 21:44:24 -0400

    Setup Direction class and tests

---
+++ models/Direction.js
@@ -0,0 +1,21 @@
+class Direction {
+  constructor(value) {
+    this.value = value;
+  }
+
+  turnRight() {
+    if (this.value === "N") {
+      this.value = "E";
+    } else if (this.value === "S") {
+      this.value = "W";
+    } else if (this.value === "E") {
+      this.value = "S";
+    } else if (this.value === "W") {
+      this.value = "N";
+    } else {
+      throw new Error("not a valid directional value");
+    }
+  }
+}
+
+module.exports = Direction;
---
+++ test/Direction.test.js
@@ -0,0 +1,26 @@
+const Direction = require("../models/Direction");
+
+test("should have a directional value", () => {
+  const value = "N";
+  const direction = new Direction(value);
+
+  expect(direction.value).toBe(value);
+});
+
+test("should have the right value after turning right", () => {
+  const directionN = new Direction("N");
+  const directionS = new Direction("S");
+  const directionE = new Direction("E");
+  const directionW = new Direction("W");
+
+  const directions = [directionN, directionS, directionE, directionW];
+
+  directions.forEach((dir) => dir.turnRight());
+
+  const actualValues = directions.map((dir) => dir.value);
+  const expectedValues = ["E", "W", "S", "N"];
+
+  actualValues.forEach((actualValue, index) => {
+    expect(expectedValues[index]).toBe(actualValue);
+  });
+});
