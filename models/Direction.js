9533c84931240245098b8cb388b13c7db7b56485
KC Kai <kirstenckai@gmail.com>
2020-06-17 22:36:53 -0400

    Add turnLeft method

--- models/Direction.js
+++ models/Direction.js
@@ -16,6 +16,20 @@ class Direction {
       throw new Error("not a valid directional value");
     }
   }
+
+  turnLeft() {
+    if (this.value === "N") {
+      this.value = "W";
+    } else if (this.value === "S") {
+      this.value = "E";
+    } else if (this.value === "E") {
+      this.value = "N";
+    } else if (this.value === "W") {
+      this.value = "S";
+    } else {
+      throw new Error("not a valid directional value");
+    }
+  }
 }
 
 module.exports = Direction;
--- test/Direction.test.js
+++ test/Direction.test.js
@@ -1,26 +1,46 @@
 const Direction = require("../models/Direction");
 
-test("should have a directional value", () => {
-  const value = "N";
-  const direction = new Direction(value);
+describe("Direction", () => {
+  test("should have a directional value", () => {
+    const value = "N";
+    const direction = new Direction(value);
 
-  expect(direction.value).toBe(value);
-});
+    expect(direction.value).toBe(value);
+  });
 
-test("should have the right value after turning right", () => {
-  const directionN = new Direction("N");
-  const directionS = new Direction("S");
-  const directionE = new Direction("E");
-  const directionW = new Direction("W");
+  test("should have the right value after turning right", () => {
+    const directionN = new Direction("N");
+    const directionS = new Direction("S");
+    const directionE = new Direction("E");
+    const directionW = new Direction("W");
 
-  const directions = [directionN, directionS, directionE, directionW];
+    const directions = [directionN, directionS, directionE, directionW];
 
-  directions.forEach((dir) => dir.turnRight());
+    directions.forEach((dir) => dir.turnRight());
 
-  const actualValues = directions.map((dir) => dir.value);
-  const expectedValues = ["E", "W", "S", "N"];
+    const actualValues = directions.map((dir) => dir.value);
+    const expectedValues = ["E", "W", "S", "N"];
 
-  actualValues.forEach((actualValue, index) => {
-    expect(expectedValues[index]).toBe(actualValue);
+    actualValues.forEach((actualValue, index) => {
+      expect(expectedValues[index]).toBe(actualValue);
+    });
+  });
+
+  test("should have the right value after turning left", () => {
+    const directionN = new Direction("N");
+    const directionS = new Direction("S");
+    const directionE = new Direction("E");
+    const directionW = new Direction("W");
+
+    const directions = [directionN, directionS, directionE, directionW];
+
+    directions.forEach((dir) => dir.turnLeft());
+
+    const actualValues = directions.map((dir) => dir.value);
+    const expectedValues = ["W", "E", "N", "S"];
+
+    actualValues.forEach((actualValue, index) => {
+      expect(expectedValues[index]).toBe(actualValue);
+    });
   });
 });
